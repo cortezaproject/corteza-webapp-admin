783b78626281ef89688faf482c0dcb3529e03181
Tit Petric <black@scene-si.org>
2019-01-29 23:18:41 +0000

    upd: build config for /admin to match crm

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,30 @@
 var webpack = require('webpack')
 var exec = require('child_process').execSync
 
+const baseUrl = process.env.NODE_ENV === 'production' ? '/admin' : '/'
+
+let optimization
+if (process.env.NODE_ENV !== 'test') {
+  // Enabling optimization when running unit testing confuses
+  // mocha (it does not find any tests)
+  optimization = {
+    usedExports: true,
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  }
+}
+
 module.exports = {
-  baseUrl: process.env['NODE_ENV'] === 'production' ? '/admin/' : '/',
-  lintOnSave: false,
+  baseUrl,
+  lintOnSave: true,
   configureWebpack: {
     // other webpack options to merge in ...
     plugins: [
@@ -12,7 +33,18 @@ module.exports = {
         CRUST_BUILD_TIME: JSON.stringify((new Date()).toISOString()),
       }),
     ],
+
+    optimization,
   },
+
+  // Do not copy config files (deployment procedure will do that)
+  chainWebpack: config => {
+    config.plugin('copy').tap(options => {
+      options[0][0].ignore.push('config*js')
+      return options
+    })
+  },
+
   // devServer Options don't belong into `configureWebpack`
   devServer: {
     host: '0.0.0.0',
@@ -20,4 +52,7 @@ module.exports = {
     disableHostCheck: true,
   },
   runtimeCompiler: true,
+  css: {
+    sourceMap: process.env['NODE_ENV'] === 'development',
+  },
 }
