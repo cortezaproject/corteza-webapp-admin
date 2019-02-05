137b97386440d4ebb25b921c14b4ed6cf590facd
Denis Arh <denis.arh@kendu.si>
2019-02-05 08:37:00 +0100

    Config cleanup, migrate to external api libs

---
+++ public/config.example.js
@@ -0,0 +1,6 @@
+window.CrustConfig = {
+  system: { baseUrl: 'https://system.api.latest.crust.tech' },
+  messaging: { baseUrl: 'https://messaging.api.latest.crust.tech' },
+  crm: { baseUrl: 'https://crm.api.latest.crust.tech' },
+  webapp: { baseUrl: 'http://webapp.local.crust.tech:8080' },
+}
