b1945d4cf69493e6591acdaf4cb27ed5da189af1
Jože Fortun <joze.fortun@gmail.com>
2019-11-21 18:48:42 +0100

    Add Compose permissions

--- src/i18n/en/compose/permissions.js
+++ src/i18n/en/compose/permissions.js
@@ -2,4 +2,98 @@ export default {
   navItem: {
     label: 'Permissions',
   },
+
+  list: {
+    title: 'System permissions',
+
+    rules: {
+      title: 'List of rules',
+      loading: 'Loading permissions',
+      submit: 'Submit',
+
+      compose: {
+        title: 'Compose service',
+
+        access: 'Access to compose',
+        grant: 'Grant permissions on compose service',
+        namespace: {
+          title: 'Namespaces',
+
+          create: 'Create namespaces',
+          read: 'Read-access any namespace',
+          manage: 'Manage any namespace',
+          update: 'Update any namespace',
+          delete: 'Delete any namespace',
+          module: {
+            create: 'Create modules under any namespace',
+          },
+          chart: {
+            create: 'Create charts under any namespace',
+          },
+          page: {
+            create: 'Create pages under any namespace',
+          },
+          'automation-script': {
+            create: 'Create automation script under any namespace',
+          },
+        },
+        settings: {
+          read: 'Access all settings',
+          manage: 'Manage all settings',
+        },
+        module: {
+          title: 'Modules',
+
+          read: 'Read any module',
+          update: 'Update any module',
+          delete: 'Delete any module',
+          record: {
+            create: 'Create record under any module',
+            read: 'Read records under any module',
+            update: 'Update records under any module',
+            delete: 'Delete records under any module',
+          },
+          'automation-trigger': {
+            manage: 'Manage any automation trigger',
+          },
+        },
+        'module-field': {
+          title: 'Module fields',
+
+          record: {
+            value: {
+              read: 'Read any module field',
+              update: 'Update any module field',
+            },
+          },
+        },
+        page: {
+          title: 'Pages',
+
+          read: 'Read any page',
+          update: 'Update any page',
+          delete: 'Delete any page',
+        },
+        chart: {
+          title: 'Charts',
+
+          read: 'Read any chart',
+          update: 'Update any chart',
+          delete: 'Delete any chart',
+        },
+        'automation-script': {
+          title: 'Automation scripts',
+
+          read: 'Read any automation script',
+          update: 'Update any automation script',
+          delete: 'Delete any automation script',
+        },
+        'automation-trigger': {
+          title: 'Automation triggers',
+
+          run: 'Run any automation trigger',
+        },
+      },
+    },
+  },
 }
