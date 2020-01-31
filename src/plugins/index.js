0ac3f419d2af36823cb262e302444ae1fbccbe3d
Denis Arh <denis.arh@gmail.com>
2020-01-31 18:47:59 +0100

    Update plugin registration

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,6 +6,8 @@ import Vuex from 'vuex'
 
 import { plugins } from '@cortezaproject/corteza-vue'
 
+import pairs from './eventbus-pairs'
+
 Vue.use(BootstrapVue)
 Vue.use(Router)
 Vue.use(Vuex)
@@ -13,6 +15,20 @@ Vue.use(Vuex)
 Vue.use(plugins.CortezaAPI('compose'))
 Vue.use(plugins.CortezaAPI('system'))
 Vue.use(plugins.CortezaAPI('messaging'))
-Vue.use(plugins.Auth(Vue.prototype.$SystemAPI))
-Vue.use(plugins.EventBus())
-Vue.use(plugins.UIHooks('admin'))
+
+const notProduction = (process.env.NODE_ENV !== 'production')
+
+Vue.use(plugins.EventBus(), {
+  strict: notProduction,
+  verbose: notProduction,
+  pairs,
+})
+
+Vue.use(plugins.UIHooks(), {
+  app: 'compose',
+  verbose: notProduction,
+})
+
+Vue.use(plugins.Auth(), {
+  api: Vue.prototype.$SystemAPI,
+})
