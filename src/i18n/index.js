87b04b3eed10d4f8a34eea736e9fc5136bf1d550
fajfa <joze.fortun@gmail.com>
2021-06-08 12:15:59 +0200

    Add i18next-pseudo

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import i18next from 'i18next'
 import lngDetector from 'i18next-browser-languagedetector'
 import VueI18Next from '@panter/vue-i18next'
+import moment from 'moment'
+import Pseudo from 'i18next-pseudo'
 
 import en from './en'
 
@@ -26,7 +28,18 @@ export default (lng = 'en', fallbackLng = lng) => {
 
   i18next.use(lngDetector).init(options)
 
+  i18next
+    .use(new Pseudo({
+      enabled: process.env.NODE_ENV !== 'production',
+    }))
+    .init({
+      postProcess: ['pseudo'],
+    })
+
   Vue.use(VueI18Next)
+
+  // Set locales for other libs we use:
+  moment.locale(lng)
 
   return new VueI18Next(i18next)
 }
