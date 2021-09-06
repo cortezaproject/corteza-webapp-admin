bc1a3f04845ad5ad8ff3569ecdbbe9e1a1aa0b42
Denis Arh <denis.arh@gmail.com>
2021-09-06 07:29:11 +0200

    Reorganize navigation translations

--- src/components/CTheMainNav.vue
+++ src/components/CTheMainNav.vue
@@ -32,79 +32,81 @@ export default {
     CSidebarNavItems,
   },
 
+  i18nOptions: {
+    namespaces: 'navigation',
+  },
+
   data () {
     return {
       nav: [
         {
           items: [
             {
-              label: 'dashboard:navItem.label',
+              label: 'dashboard',
               route: 'dashboard',
               icon: 'tachometer-alt',
             },
           ],
         },
         {
-          header: {
-            label: 'system:navGroup.label',
-          },
+          header: { label: 'system.group' },
           items: [
             {
-              label: 'system.users:navItem.label',
+              label: 'system.items.users',
               route: 'system.user',
               icon: 'users',
               can: ['system/', 'users.search'],
             },
             {
-              label: 'system.roles:navItem.label',
+              label: 'system.items.roles',
               route: 'system.role',
               icon: 'hat-cowboy',
               can: ['system/', 'roles.search'],
             },
             {
-              label: 'system.applications:navItem.label',
+              label: 'system.items.applications',
               route: 'system.application',
               icon: 'th-large',
               can: ['system/', 'applications.search'],
             },
             {
-              label: 'system.templates:navItem.label',
+              label: 'system.items.templates',
               route: 'system.template',
               icon: 'file-code',
               can: ['system/', 'templates.search'],
             },
             {
-              label: 'system.settings:navItem.label',
+              label: 'system.items.settings',
               route: 'system.settings',
               icon: 'sliders-h',
               can: ['system/', 'settings.read'],
             },
             {
-              label: 'system.permissions:navItem.label',
+              label: 'system.items.permissions',
               route: 'system.permissions',
               icon: 'lock',
               can: ['system/', 'grant'],
             },
             {
-              label: 'system.authclients:navItem.label',
+              label: 'system.items.authclients',
               route: 'system.authClient',
               icon: 'key',
               can: ['system/', 'auth-clients.search'],
             },
             {
-              label: 'system.actionlog:navItem.label',
+              label: 'system.items.actionlog',
               route: 'system.actionlog',
               icon: 'glasses',
               can: ['system/', 'action-log.read'],
             },
             {
-              label: 'system.queues:navItem.label',
+              label: 'system.items.queues',
               route: 'system.queue',
               icon: 'stream',
               can: ['system/', 'queues.search'],
             },
             {
-              label: 'system.apigw:navItem.label',
+              label: 'system.items.apigw',
               route: 'system.apigw',
               icon: 'archway',
               can: ['system/', 'apigw-routes.search'],
@@ -113,17 +115,17 @@ export default {
         },
 
         {
-          header: { label: 'compose:navGroup.label' },
+          header: { label: 'compose.group' },
           items: [
             {
-              label: 'compose.settings:navItem.label',
+              label: 'compose.items.settings',
               route: 'compose.settings',
               icon: 'sliders-h',
               can: ['compose/', 'settings.read'],
             },
 
             {
-              label: 'compose.permissions:navItem.label',
+              label: 'compose.items.permissions',
               route: 'compose.permissions',
               icon: 'lock',
               can: ['compose/', 'grant'],
@@ -132,31 +134,31 @@ export default {
         },
 
         {
-          header: { label: 'automation:navGroup.label' },
+          header: { label: 'automation.group' },
           items: [
             {
-              label: 'automation.workflows:navItem.label',
+              label: 'automation.items.workflows',
               route: 'automation.workflow',
               icon: 'project-diagram',
               can: ['automation/', 'workflows.search'],
             },
 
             {
-              label: 'automation.sessions:navItem.label',
+              label: 'automation.items.sessions',
               route: 'automation.session',
               icon: 'business-time',
               can: ['automation/', 'sessions.search'],
             },
 
             {
-              label: 'automation.scripts:navItem.label',
+              label: 'automation.items.scripts',
               route: 'automation.scripts',
               icon: 'scroll',
               can: ['automation/', 'workflows.search'],
             },
 
             {
-              label: 'automation.permissions:navItem.label',
+              label: 'automation.items.permissions',
               route: 'automation.permissions',
               icon: 'lock',
               can: ['automation/', 'grant'],
@@ -164,16 +166,16 @@ export default {
           ],
         },
         {
-          header: { label: 'federation:navGroup.label' },
+          header: { label: 'federation.group' },
           items: [
             {
-              label: 'federation.nodes:navItem.label',
+              label: 'federation.items.nodes',
               route: 'federation.nodes',
               icon: 'share-alt',
               can: ['federation/', 'pair'],
             },
             {
-              label: 'federation.permissions:navItem.label',
+              label: 'federation.items.permissions',
               route: 'federation.permissions',
               icon: 'lock',
               can: ['federation/', 'grant'],
@@ -181,10 +183,10 @@ export default {
           ],
         },
         {
-          header: { label: 'ui:navGroup.label' },
+          header: { label: 'ui.group' },
           items: [
             {
-              label: 'ui.settings:navItem.label',
+              label: 'ui.items.settings',
               route: 'ui.settings',
               icon: 'eye',
               can: ['system/', 'settings.read'],
