18c051739fd9a62e5ab224ae96b5563066671eed
Katrin Yordanova <katrin.yordanova@crust.tech>
2022-04-12 11:01:01 +0300

    Add DataSources, ExternaDataSources and RecordFederation components

---
+++ src/views/System/DataSources/Create.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container
+    class="py-3"
+  >
+    <c-content-header
+      :title="$t('external.title.create')"
+    />
+
+    <c-external-data-sources-editor-info
+      class="mt-4"
+    />
+  </b-container>
+</template>
+
+<script>
+import CExternalDataSourcesEditorInfo from 'corteza-webapp-admin/src/components/DataSources/External/CExternalDataSourcesEditorInfo'
+
+export default {
+  i18nOptions: {
+    namespaces: 'system.datasources',
+  },
+
+  components: {
+    CExternalDataSourcesEditorInfo,
+  },
+}
+</script>
