a3afa01f6c3decfdd44b174274b42f349f4463dd
Jože Fortun <joze.fortun@gmail.com>
2022-05-23 17:01:35 +0200

    Rename Datasource to Connection

---
+++ src/views/System/Connection/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container
+    class="py-3"
+  >
+    <c-content-header
+      :title="$t('title')"
+    />
+
+    <c-connection-basic-info
+      :connection="connection"
+      class="mb-4"
+    />
+
+    <c-external-connection-list
+      class="mb-4"
+    />
+
+    <c-federated-node-list />
+  </b-container>
+</template>
+<script>
+import CConnectionBasicInfo from 'corteza-webapp-admin/src/components/Connection/CConnectionBasicInfo'
+import CExternalConnectionList from 'corteza-webapp-admin/src/components/Connection/External/CExternalConnectionList'
+import CFederatedNodeList from 'corteza-webapp-admin/src/components/Connection/CFederatedNodeList'
+
+export default {
+  name: 'Connection',
+
+  components: {
+    CConnectionBasicInfo,
+    CExternalConnectionList,
+    CFederatedNodeList,
+  },
+
+  i18nOptions: {
+    namespaces: 'system.connections',
+    keyPrefix: 'list',
+  },
+
+  data () {
+    return {
+      connection: {
+        connectionID: '1',
+        location: 'Ireland',
+        ownership: 'ACME Ltd.',
+      },
+    }
+  },
+}
+</script>
