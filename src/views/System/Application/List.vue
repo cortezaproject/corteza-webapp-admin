4ec99053b09a09cc63cac278d44f9481cb671bd6
Jože Fortun <joze.fortun@gmail.com>
2022-09-08 12:58:14 +0200

    Add default sorting to resource lists

--- src/views/System/Application/List.vue
+++ src/views/System/Application/List.vue
@@ -119,6 +119,11 @@ export default {
         deleted: 0,
       },
 
+      sorting: {
+        sortBy: 'createdAt',
+        sortDesc: true,
+      },
+
       fields: [
         {
           key: 'name',
