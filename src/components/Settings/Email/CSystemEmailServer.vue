cca4c3e87571d8e69e643e274f479e31c2b0bae3
Katrin Yordanova <katrin.yordanova@crust.tech>
2022-09-19 17:04:26 +0300

    Add cypress ids to System/Email Settings

--- src/components/Settings/Email/CSystemEmailServer.vue
+++ src/components/Settings/Email/CSystemEmailServer.vue
@@ -15,6 +15,7 @@
         <b-input-group>
           <b-input
             v-model="server.host"
+            data-test-id="input-server"
             :disabled="disabled"
             placeholder="host.domain.tld"
             autocomplete="off"
@@ -26,6 +27,7 @@
           <b-input-group-append>
             <b-input
               v-model="server.port"
+              data-test-id="input-server-port"
               type="number"
               :disabled="disabled"
               step="1"
@@ -41,6 +43,7 @@
       >
         <b-input
           v-model="server.user"
+          data-test-id="input-user"
           :disabled="disabled"
           autocomplete="off"
         />
@@ -52,6 +55,7 @@
       >
         <b-input
           v-model="server.pass"
+          data-test-id="input-password"
           type="password"
           :disabled="disabled"
           autocomplete="off"
@@ -67,6 +71,7 @@
       >
         <b-input
           v-model="server.from"
+          data-test-id="input-sender-address"
           type="email"
           :disabled="disabled"
           autocomplete="off"
@@ -81,6 +86,7 @@
       >
         <b-form-checkbox
           v-model="server.tlsInsecure"
+          data-test-id="checkbox-allow-invalid-certificates"
           :disabled="disabled"
         >
           {{ $t('tlsInsecure.label') }}
@@ -93,6 +99,7 @@
       >
         <b-input
           v-model="server.tlsServerName"
+          data-test-id="input-tls-server-name"
           :disabled="disabled"
         />
       </b-form-group>
